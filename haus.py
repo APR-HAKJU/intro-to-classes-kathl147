4f2af1365f2019029ea5800d2c783b23b876fef7
kathl <wolfsbergerkatja10@gmail.com>
2025-11-25 11:27:10 +0100

    Added haus

--- haus.py
+++ haus.py
@@ -3,14 +3,25 @@
 # Aktuell werden die Eigenschaften als separate Variablen definiert.
 # Definiere einen Konstruktor (__init__), um die Attribute zu initialisieren.
 class Haus:
-    pass
+    def __init__(self, quadratmeter, schlafzimmer, badezimmer ):
+        self.quadratmeter=quadratmeter
+        self.schlafzimmer=schlafzimmer
+        self.badezimmer=badezimmer
+        print("Neues Haus wurde erstellt!")
 
-haus1 = Haus()
-haus1.quadratmeter = 120
-haus1.schlafzimmer = 3
-haus1.badezimmer = 2
+haus1 = Haus(quadratmeter=120, schlafzimmer=3, badezimmer=2)
+haus2=Haus(quadratmeter=85, schlafzimmer=4, badezimmer=1)
+haus3=Haus(quadratmeter=200, schlafzimmer=3, badezimmer=4)
 
 print(f"Haus: {haus1.quadratmeter}m², {haus1.schlafzimmer} Schlafzimmer")
+print(f"Haus: {haus2.quadratmeter}m², {haus2.schlafzimmer} Schlafzimmer")
+print(f"Haus: {haus3.quadratmeter}m², {haus3.schlafzimmer} Schlafzimmer")
+
+haus4=Haus(quadratmeter=140, schlafzimmer=4, badezimmer=3)
+print(f"Haus 4 hat {haus4.quadratmeter}m²")
+
+
+
 
 # TODO: Aufgabe 2: Erstelle drei weitere Objekte der Klasse Haus mit unterschiedlichen Eigenschaften.
 
